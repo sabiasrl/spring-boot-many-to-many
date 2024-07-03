ddd531af29514ad55e4bc6abd9cf18a9d8dcd143
SabiaSRL <daniele.sabia.dev@hotmail.com>
2024-07-03 10:20:09 +0200

    initial commit

--- src/test/java/com/sabiasrl/spring/jpa/h2/manytomany/it/ManyToManyEmbeddedIdIntegrationTest.java
+++ src/test/java/com/sabiasrl/spring/jpa/h2/manytomany/it/ManyToManyEmbeddedIdIntegrationTest.java
@@ -26,29 +26,21 @@ ManyToManyEmbeddedIdIntegrationTest {
         var a11 = new A(new AKey("partition1", "sort1"), "a11");
         var b11 = new B(new BKey("partition1", "sort1"), "b11");
 
-//        var a22 = new A(new AKey("partition2", "sort2"), "value22");
-//
-//        var b22 = new B(new BKey("partition2", "sort2"), "value22");
-
         entityManager.persist(a11);
         entityManager.persist(b11);
-//        entityManager.persist(a22);
-//        entityManager.persist(b22);
 
         b11.getASet().add(a11);
-
-        Assertions.assertThat(entityManager.find(A.class, a11.getAKey())).isNotNull();
-        Assertions.assertThat(entityManager.contains(a11)).isTrue();
-        Assertions.assertThat(entityManager.contains(b11)).isTrue();
 
         entityManager.flush();
         entityManager.detach(b11);
         entityManager.detach(a11);
+
         var b11Refreshed = entityManager.find(B.class, b11.getBKey());
 
         Assertions.assertThat(b11Refreshed.getASet()).isNotEmpty();
         Assertions.assertThat(b11Refreshed.getASet()).hasSize(1);
         Assertions.assertThat(b11Refreshed.getASet()).containsExactly(a11);
+
         var a11Refreshed = b11Refreshed.getASet().stream().findFirst().orElse(null);
         Assertions.assertThat(a11Refreshed).isNotNull();
         Assertions.assertThat(a11Refreshed.getName()).isEqualTo("a11");
@@ -57,5 +49,4 @@ ManyToManyEmbeddedIdIntegrationTest {
         Assertions.assertThat(a11Refreshed.getBSet()).hasSize(1);
         Assertions.assertThat(a11Refreshed.getBSet()).containsExactly(b11Refreshed);
     }
-
 }
